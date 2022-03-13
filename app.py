0ac134fd36f9641e4048dfa7753208da4cc7c1a3
Kelly Brazil <kellyjonbrazil@gmail.com>
2022-03-13 13:03:34 -0700

    use new public api, bump to v1.18.5

--- app.py
+++ app.py
@@ -3,7 +3,6 @@
 
 import sys
 import os
-import importlib
 import json
 from pygments import highlight
 from pygments.lexers import JsonLexer
@@ -12,8 +11,8 @@ from flask import Flask, render_template, flash
 from flask_wtf import FlaskForm
 from wtforms.fields import TextAreaField, SelectField, BooleanField, SubmitField
 from wtforms.validators import DataRequired
-from jc.cli import info as jc_info
-from jc.cli import about_jc
+from jc import __version__, standard_parser_mod_list, parse
+
 
 TITLE = 'jc web'
 DEBUG = False
@@ -27,13 +26,6 @@ else:
     app.config['SECRET_KEY'] = 'deadbeef'
     print('Using development key', file=sys.stderr)
 
-# convert parser list names to module names
-parser_mod_list = []
-for parser in about_jc()['parsers']:
-    # do not include streaming parsers
-    if not parser.get('streaming', None):
-        parser_mod_list.append(parser['name'])
-
 
 # --- ROUTES ---
 
@@ -44,11 +36,18 @@ def home():
     output = ''
     if form.validate_on_submit():
         try:
-            parser = importlib.import_module('jc.parsers.' + form.command_parser.data)
-            output = parser.parse(form.command_output.data, quiet=True, raw=form.raw_json.data)
+            # parser = importlib.import_module('jc.parsers.' + form.command_parser.data)
+            output = parse(form.command_parser.data,
+                           form.command_output.data,
+                           quiet=True,
+                           raw=form.raw_json.data)
         except Exception:
             flash('jc was unable to parse the content. Did you use the correct parser?', 'danger')
-            return render_template('home.html', title=TITLE, jc_info=jc_info, form=form, output=output)
+            return render_template('home.html',
+                                   title=TITLE,
+                                   jc_version=__version__,
+                                   form=form,
+                                   output=output)
 
         if form.pretty_print.data:
             output = json.dumps(output, indent=2)
@@ -56,14 +55,18 @@ def home():
             output = json.dumps(output)
         output = highlight(output, JsonLexer(), HtmlFormatter(noclasses=True))
 
-    return render_template('home.html', title=TITLE, jc_info=jc_info, form=form, output=output)
+    return render_template('home.html',
+                           title=TITLE,
+                           jc_version=__version__,
+                           form=form,
+                           output=output)
 
 
 # --- FORMS ---
 
 
 class MyInput(FlaskForm):
-    command_parser = SelectField('Parser', choices=parser_mod_list)
+    command_parser = SelectField('Parser', choices=standard_parser_mod_list())
     command_output = TextAreaField('Command Output', validators=[DataRequired()])
     pretty_print = BooleanField('Pretty Print', default='checked')
     raw_json = BooleanField('Raw JSON')
